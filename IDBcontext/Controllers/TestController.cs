54458385ce289a95ab910eca4aea506898b6bb14
ArashJafary <aj2774382@gmail.com>
2023-05-06 17:07:42 +0430

    add a one get user

--- IDBcontext/Controllers/TestController.cs
+++ IDBcontext/Controllers/TestController.cs
@@ -3,6 +3,7 @@ using System.Data;
 using testIDBcon.infrastructure;
 using testIDBcon.Model;
 using testIDBcon.Model.DTO;
+using Newtonsoft.Json;
 using testIDBcon.Model.Repository;
 
 namespace testIDBcon.Controllers
@@ -34,8 +35,32 @@ namespace testIDBcon.Controllers
         {
             using (IDbConnection Connection = _context.Create())
             {
-                var Data = Connection.Exqut<User>("select * from USER_1 ");
-                return Ok(Data);
+                var Data = Connection.Exqut<UserGetDTO>("select * from USER_1 where Id=@input1",2);
+                var Data2 = Data.Select(a=>
+                new User(){
+                    Id=a.Id,
+                    UserName=a.UserName,
+                    PhonNumbers= JsonConvert.DeserializeObject<List<PhoneNumber>>(a.PhonNumbers)
+                }
+                );
+                return Ok(Data2);
+            }
+        }
+
+
+      [HttpGet("[action]",Name=nameof(TestExtension2))]
+        public  IActionResult TestExtension2()
+        {
+            using (IDbConnection Connection = _context.Create())
+            {
+                var Data = Connection.ExqutSingelOrDefault<UserGetDTO>("select * from USER_1 Where Id=@input1",2);
+                var Data2 =
+                new User(){
+                    Id=Data.Id,
+                    UserName=Data.UserName,
+                    PhonNumbers= JsonConvert.DeserializeObject<List<PhoneNumber>>(Data.PhonNumbers)
+                };
+                return Ok(Data2);
             }
         }
     }
